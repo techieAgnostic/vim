c7b44237326baa655379f94a2e1b596d3ecba72c
Thorn Avery <s@p7.co.nz>
2021-04-09 11:18:41 +1200

    better racket config

--- overlay.nix
+++ overlay.nix
@@ -7,15 +7,9 @@ customPlugins: self: super: {
           knownPlugins = super.vimPlugins // (customPlugins super);
           pluginNames = [
             "vim-racket"
-            "haskell-vim"
             "rainbow-vim"
             "vimwiki"
-            "vim-sexp-mappings-for-regular-people"
-            "vim-sexp"
-            "vim-surround"
-            "vim-repeat"
             "slimv"
-            "idris-vim"
           ];
         };
         customRC = ''
@@ -43,17 +37,6 @@ customPlugins: self: super: {
           set backspace=eol,indent,start
           set tabstop=2 softtabstop=2 expandtab shiftwidth=2 smarttab
       
-          autocmd BufNewFile,BufRead *.rkt set filetype=scheme
-          autocmd FileType scheme :packadd vim-sexp
-          autocmd FileType scheme :packadd vim-sexp-for-regular-people
-          autocmd FileType scheme :packadd vim-surround
-          autocmd FileType scheme :packadd vim-repeat
-          autocmd FileType scheme :packadd slimv
-          autocmd FileType scheme :packadd vim-racket
-      
-          autocmd BufNewFile,BufRead *.hoon set filetype=hoon
-          autocmd FileType hoon :packadd hoon-vim
-
           let g:rainbow_active = 1
           let g:rainbow_ctermfgs = [ 'lightblue', 'lightgreen', 'yellow', 'red', 'magenta']
         '';
