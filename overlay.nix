28ee4ba605979bda3bd05092730deb94a110dc22
Thorn Avery <s@p7.co.nz>
2021-06-02 03:12:39 +0000

    i hate this

--- overlay.nix
+++ overlay.nix
@@ -10,7 +10,6 @@ customPlugins: self: super: {
             "vimwiki"
             "slimv"
             "vim-racket"
-            "idris2"
           ];
         };
         customRC = ''
@@ -25,7 +24,6 @@ customPlugins: self: super: {
 
           if has("autocmd")
             au BufReadPost *.rkt,*.rktl set filetype=racket
-            au BufReadPost *.lidr set filetype=idris
             au filetype racket set lisp
             au filetype racket set autoindent
           endif
